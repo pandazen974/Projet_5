756b1e5fd63124b276e2671464ed721581098aad
onzol <onzol@LAPTOP-GDLSB58Q>
2019-01-21 20:20:34 +0100

    Creation model pour les cours de l'emploi du temps

---
+++ model/Classes.php
@@ -0,0 +1,107 @@
+<?php
+
+class Jobs{
+    private $id;
+    private $className;
+    private $schedule;
+    private $teacherId;
+    private $studentId;
+        
+public function __construct(array $donnees)
+  {
+    $this->hydrate($donnees);
+  }
+  
+public function hydrate(array $donnees)
+{
+  foreach ($donnees as $key => $value)
+  {
+    // nom du setter correspondant à l'attribut
+    $method = 'set'.ucfirst($key);
+        
+    // Si le setter correspondant existe.
+    if (method_exists($this, $method))
+    {
+      // On appelle le setter.
+      $this->$method($value);
+    }
+  }
+}
+
+//GETTERS
+public function id()
+{
+      
+    return $this->_id;
+      
+}
+  
+public function className()
+{
+
+    return $this->_className;
+
+}
+
+public function schedule()
+{
+
+    return $this->_schedule;
+
+}
+
+public function teacherId()
+{
+
+    return $this->_teacherId;
+
+}
+
+public function studentId()
+{
+
+    return $this->_studentId;
+
+}
+
+
+  //SETTERS
+public function setId($id)
+{
+
+    $this->_id = $id;
+
+}
+
+public function setClassName($className)
+{
+
+    $this->_className = $className;
+
+}
+
+public function setSchedule($schedule)
+{
+
+    $this->_schedule = $schedule;
+        
+}
+
+public function setTeacherId($teacherId)
+{
+      
+    $this->_teacherId=$teacherId;
+      
+}
+  
+public function setStudentId($studentId)
+{
+      
+    $this->_studentId=$studentId;
+      
+}
+
+
+
+}
+
