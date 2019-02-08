4a9ccac4a16e6022aa6c0340c6923f81cbd8c1e7
onzol <onzol@LAPTOP-GDLSB58Q>
2019-02-08 10:12:17 +0100

    création fichier Database.php pour connexion à BDD

---
+++ model/config/Database.php
@@ -0,0 +1,23 @@
+<?php
+class Database{
+  
+    protected $host = "localhost";
+    protected $db_name = "dtsimrt";
+    protected $username = "root";
+    protected $password = "";
+    
+  
+    
+    public function getConnection(){
+  
+        $this->conn = null;
+  
+        try{
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION ));
+        }catch(PDOException $exception){
+            echo "Connection error: " . $exception->getMessage();
+        }
+  
+        return $this->conn;
+    }
+}
