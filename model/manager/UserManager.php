22524e144b1da3bc876b53684c6cd95db677ec46
onzol <onzol@LAPTOP-GDLSB58Q>
2019-02-14 10:09:01 +0100

    Correction et meilleure indentation code

--- model/manager/UserManager.php
+++ model/manager/UserManager.php
@@ -12,32 +12,32 @@ Class UserManager extends Database{
     
     public function createUser(User $user){
         try{
-        $query = "INSERT INTO
+            $query = "INSERT INTO
                     " . $this->table_name . "
                 SET
                  lastName=:lastName, firstName=:firstName, middleName=:middleName, address=:address, city=:city, postalCode=:postalCode, telNumber=:telNumber, birth=:birth, email=:email, registrationDate=NOW()";
-        $stmt = $this->conn->prepare($query);
-        $lastName= htmlspecialchars($user->lastName());
-        $firstName= htmlspecialchars($user->firstName());
-        $middleName= htmlspecialchars($user->middleName());
-        $address= htmlspecialchars($user->address());
-        $city= htmlspecialchars($user->city());
-        $postalCode= htmlspecialchars($user->postalCode());
-        $telNumber=htmlspecialchars($user->telNumber());
-        $birth=htmlspecialchars($user->birth());
-        $email= htmlspecialchars($user->email());
-        $stmt->bindParam(':lastName',$lastName,PDO::PARAM_STR);
-        $stmt->bindParam(':firstName',$firstName,PDO::PARAM_STR);
-        $stmt->bindParam(':middleName',$middleName,PDO::PARAM_STR);
-        $stmt->bindParam(':address',$address,PDO::PARAM_STR);
-        $stmt->bindParam(':city',$city,PDO::PARAM_STR);
-        $stmt->bindParam(':postalCode',$postalCode,PDO::PARAM_INT);
-        $stmt->bindParam(':telNumber',$telNumber);
-        $stmt->bindParam(':birth',$birth,PDO::PARAM_STR);
-        $stmt->bindParam(':email',$email,PDO::PARAM_STR);
-        $stmt->execute();
-        $user->setId($this->conn->lastInsertId());
-        return $user;
+            $stmt = $this->conn->prepare($query);
+            $lastName= htmlspecialchars($user->lastName());
+            $firstName= htmlspecialchars($user->firstName());
+            $middleName= htmlspecialchars($user->middleName());
+            $address= htmlspecialchars($user->address());
+            $city= htmlspecialchars($user->city());
+            $postalCode= htmlspecialchars($user->postalCode());
+            $telNumber=htmlspecialchars($user->telNumber());
+            $birth=htmlspecialchars($user->birth());
+            $email= htmlspecialchars($user->email());
+            $stmt->bindParam(':lastName',$lastName,PDO::PARAM_STR);
+            $stmt->bindParam(':firstName',$firstName,PDO::PARAM_STR);
+            $stmt->bindParam(':middleName',$middleName,PDO::PARAM_STR);
+            $stmt->bindParam(':address',$address,PDO::PARAM_STR);
+            $stmt->bindParam(':city',$city,PDO::PARAM_STR);
+            $stmt->bindParam(':postalCode',$postalCode,PDO::PARAM_INT);
+            $stmt->bindParam(':telNumber',$telNumber);
+            $stmt->bindParam(':birth',$birth,PDO::PARAM_STR);
+            $stmt->bindParam(':email',$email,PDO::PARAM_STR);
+            $stmt->execute();
+            $user->setId($this->conn->lastInsertId());
+            return $user;
         
         }
         
@@ -47,8 +47,8 @@ Class UserManager extends Database{
     }
     
     public function readAllUsers(){
-    try{
-        $query = "SELECT *,DATE_FORMAT(registrationDate, '%d/%m/%Y') as registrationDate,DATE_FORMAT(birth, '%d/%m/%Y') as birth
+        try{
+            $query = "SELECT *,DATE_FORMAT(registrationDate, '%d/%m/%Y') as registrationDate,DATE_FORMAT(birth, '%d/%m/%Y') as birth
 
             FROM
                 " . $this->table_name . "
@@ -56,17 +56,17 @@ Class UserManager extends Database{
                 id
            ";
 
-    $stmt = $this->conn->prepare( $query );
-    $stmt->execute();
+            $stmt = $this->conn->prepare( $query );
+            $stmt->execute();
 
-    while($donnees=$stmt->fetch(\PDO::FETCH_ASSOC))
-    {
-        $users[]=new User($donnees);
-    }
+            while($donnees=$stmt->fetch(\PDO::FETCH_ASSOC))
+                {
+                    $users[]=new User($donnees);
+                }
 
-    return $users;
+            return $users;
 
-    }
+            }
     
         catch (Exception $e){
             exit('<b>Catched exception at line '. $e->getLine() .' :</b> '. $e->getMessage());
@@ -74,48 +74,71 @@ Class UserManager extends Database{
     
     }
     
-       public function updateUser(User $user){
+    public function updateUser(User $user){
         try{
-    $query = "UPDATE user
-             SET
+            $query = "UPDATE user
+                    SET
                     lastName=:lastName, firstName=:firstName, middleName=:middleName, address=:address, city=:city, postalCode=:postalCode, telNumber=:telNumber, birth=:birth, email=:email, registrationDate=NOW()
-                    WHERE id = :id";
-        $stmt = $this->conn->prepare($query);
-        $lastName= htmlspecialchars($user->lastName());
-        $firstName= htmlspecialchars($user->firstName());
-        $middleName= htmlspecialchars($user->middleName());
-        $address= htmlspecialchars($user->address());
-        $city= htmlspecialchars($user->city());
-        $postalCode= htmlspecialchars($user->postalCode());
-        $telNumber=htmlspecialchars($user->telNumber());
-        $birth=htmlspecialchars($user->birth());
-        $email= htmlspecialchars($user->email());
-        $stmt->bindParam(':lastName',$lastName,PDO::PARAM_STR);
-        $stmt->bindParam(':firstName',$firstName,PDO::PARAM_STR);
-        $stmt->bindParam(':middleName',$middleName,PDO::PARAM_STR);
-        $stmt->bindParam(':address',$address,PDO::PARAM_STR);
-        $stmt->bindParam(':city',$city,PDO::PARAM_STR);
-        $stmt->bindParam(':postalCode',$postalCode,PDO::PARAM_INT);
-        $stmt->bindParam(':telNumber',$telNumber);
-        $stmt->bindParam(':birth',$birth,PDO::PARAM_STR);
-        $stmt->bindParam(':email',$email,PDO::PARAM_STR);
-        $stmt->execute();
-        return $user;
-        
-        }
+                    WHERE id =:id";
+            $stmt = $this->conn->prepare($query);
+            $lastName= htmlspecialchars($user->lastName());
+            $firstName= htmlspecialchars($user->firstName());
+            $middleName= htmlspecialchars($user->middleName());
+            $address= htmlspecialchars($user->address());
+            $city= htmlspecialchars($user->city());
+            $postalCode= htmlspecialchars($user->postalCode());
+            $telNumber=htmlspecialchars($user->telNumber());
+            $birth=htmlspecialchars($user->birth());
+            $email= htmlspecialchars($user->email());
+            $id= htmlspecialchars($user->id());
+            $stmt->bindParam(':lastName',$lastName,PDO::PARAM_STR);
+            $stmt->bindParam(':firstName',$firstName,PDO::PARAM_STR);
+            $stmt->bindParam(':middleName',$middleName,PDO::PARAM_STR);
+            $stmt->bindParam(':address',$address,PDO::PARAM_STR);
+            $stmt->bindParam(':city',$city,PDO::PARAM_STR);
+            $stmt->bindParam(':postalCode',$postalCode,PDO::PARAM_INT);
+            $stmt->bindParam(':telNumber',$telNumber,PDO::PARAM_INT);
+            $stmt->bindParam(':birth',$birth,PDO::PARAM_STR);
+            $stmt->bindParam(':email',$email,PDO::PARAM_STR);
+            $stmt->bindParam(':id',$id,PDO::PARAM_INT);
+            $stmt->execute();
+            return $user;
+            }
+            
         catch (Exception $e){
             exit('<b>Catched exception at line '. $e->getLine() .' :</b> '. $e->getMessage());
         }
     }
+    
+     public function readSelectedUser($id){
+            $query = "SELECT *
+            FROM
+                " . $this->table_name . "
+            WHERE
+                id = :id
+            LIMIT
+                0,1";
+   
+            $stmt = $this->conn->prepare( $query );
+            $stmt->bindParam(':id', $id,PDO::PARAM_INT);
+            $stmt->execute();
+            $row = $stmt->fetch(\PDO::FETCH_ASSOC);
+            if($row===false){
+            $selectedUser=null;
+            }else{
+            $selectedUser=new User($row);
+            }
+            return $selectedUser;
+            }
     
     public function deleteUser(User $user){
-    try{
-    $query = "DELETE FROM user WHERE id = :id";
-    $stmt = $this->conn->prepare($query);
-    $id=$user->id();
-    $stmt->bindParam(':id', $id,PDO::PARAM_INT);
-    $stmt->execute();
-    }
+        try{
+            $query = "DELETE FROM user WHERE id = :id";
+            $stmt = $this->conn->prepare($query);
+            $id=$user->id();
+            $stmt->bindParam(':id', $id,PDO::PARAM_INT);
+            $stmt->execute();
+        }
         catch (Exception $e){
             exit('<b>Catched exception at line '. $e->getLine() .' :</b> '. $e->getMessage());
         }
