f67c6ea5572200cd7fa118127f39b5024b407d85
onzol <onzol@LAPTOP-GDLSB58Q>
2019-02-19 14:07:58 +0100

    Ajout controller pour relation UsersProfile

---
+++ controllers/UsersProfileController.php
@@ -0,0 +1,27 @@
+<?php
+class UsersProfileController extends Controller{
+    
+    public function __construct(){
+        
+        $this->smarty=parent::useSmartyFonctions();
+        
+    }
+    
+public function registerUsersProfile(){
+    $usersProfileManager=new UsersProfileManager();
+    
+                $usersProfile=new UsersProfile(['userId'=>$_GET['id'],'profileId'=>$_POST['profile']]);
+                if(($_POST['profile']==="1") OR ($_POST['profile']==="2")){
+                    $userManager=new UserManager();
+                    $users=$userManager->readAllUsers();
+                    $this->smarty->assign('users', $users);
+                    $this->smarty->display('view/usersList.tpl'); 
+                }else{
+                    $newUserProfile=$usersProfileManager->setUsersProfile($usersProfile);
+                    $this->smarty->assign('newUserProfile', $newUserProfile);
+                    var_dump($newUserProfile);
+                    $this->smarty->display('view/groupForm.tpl');
+                }
+    }
+    
+}
