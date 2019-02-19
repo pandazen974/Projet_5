11d6c49cd369a875d484777abd8079726177c7e3
onzol <onzol@LAPTOP-GDLSB58Q>
2019-02-19 14:10:15 +0100

    Ajout fonction deleteStudentGroup l.61

--- model/manager/StudentsGroupManager.php
+++ model/manager/StudentsGroupManager.php
@@ -28,8 +28,46 @@ public function setStudentsGroup(StudentsGroup $studentsGroup){
     catch (Exception $e){
         exit('<b>Catched exception at line '. $e->getLine() .' :</b> '. $e->getMessage());
     }
-
     
+}
+    
+public function readSelectedStudentGroup($userId){
+    try{
+            $query = "SELECT *
+            FROM
+                " . $this->table_name . "
+            WHERE
+                userId = :userId
+            LIMIT
+                0,1";
+   
+            $stmt = $this->conn->prepare( $query );
+            $stmt->bindParam(':userId', $userId,PDO::PARAM_INT);
+            $stmt->execute();
+            $row = $stmt->fetch(\PDO::FETCH_ASSOC);
+            if($row===false){
+            $selectedStudentGroup=null;
+            }else{
+            $selectedStudentGroup=new StudentsGroup($row);
+            }
+            return $selectedStudentGroup;
+            }
+            
+    catch (Exception $e){
+        exit('<b>Catched exception at line '. $e->getLine() .' :</b> '. $e->getMessage());
     }
-
+}
+    
+public function deleteStudentGroup(StudentsGroup $studentsGroup){
+    try{
+            $query = "DELETE FROM studentsgroup WHERE userId = :userId";
+            $stmt = $this->conn->prepare($query);
+            $userId=$studentsGroup->userId();
+            $stmt->bindParam(':userId', $userId,PDO::PARAM_INT);
+            $stmt->execute();
+        }
+    catch (Exception $e){
+            exit('<b>Catched exception at line '. $e->getLine() .' :</b> '. $e->getMessage());
+        }
+    }
 }
