e5cae7164021ce309944431b012e00053d652ee2
onzol <onzol@LAPTOP-GDLSB58Q>
2019-03-02 07:36:31 +0100

    Modif pour pouvoir placer des cours sur le planning

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -6,17 +6,29 @@ var edt=
     init: function () {
             
           $('#calendar').fullCalendar({
-            header: { center: 'agendaWeek' }, // buttons for switching between views
-            
-            
+            locale:'fr',
+            theme:true,
+            columnHeaderFormat:'ddd D/MM',
+            themeSystem: 'jquery-ui',
+                themeButtonIcons:{
+                prev: 'circle-triangle-w',
+                next: 'circle-triangle-e',
+                prevYear: 'seek-prev',
+                nextYear: 'seek-next'
+                },
+            defaultView: 'agendaWeek',
+            titleFormat:'D MMMM YYYY',
+            start:'07:00:00',
+            allDaySlot:false,
+            minTime:"07:00",
+            maxTime:"18:00",
             eventSources: [
-
-    // your event source
-    {
-      url: '/Projet_5/?action=feedEvents', // use the `url` property
-      color: 'yellow',    // an option!
-      textColor: 'black'  // an option!
-    }
+            // your event source
+            {
+                url: '/Projet_5/?action=feedEvents', // use the `url` property
+                color: 'yellow',    // an option!
+                textColor: 'black'  // an option!
+            }
 
     // any other sources...
 
