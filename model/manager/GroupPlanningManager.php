e6e180528de67dea363627d2512fe3803f2f67f0
onzol <onzol@LAPTOP-GDLSB58Q>
2019-03-02 20:47:33 +0100

    Ajout de la fonction setGroupPlaning pour attribution planning/groupe

--- model/manager/GroupPlanningManager.php
+++ model/manager/GroupPlanningManager.php
@@ -1,12 +1,34 @@
 <?php
 
-Class GroupPlanningManager{
+Class GroupPlanningManager extends Database{
     
-    private $conn;
     private $table_name = "groupPlanning";
+    protected $conn;
+    
  
-    public function __construct($db){
-        $this->conn = $db;
+    public function __construct(){
+        $this->conn=parent::getConnection();
+    }
+    
+    public function setGroupPlanning(GroupPlanning $groupPlanning){
+        try{
+            $query = "INSERT INTO
+                    " . $this->table_name . "
+                SET
+                    groupId=:groupId, planningId=:planningId";
+ 
+            $stmt = $this->conn->prepare($query);
+            $groupId= htmlspecialchars($groupPlanning->groupId());
+            $planningId= htmlspecialchars($groupPlanning->planningId());
+            $stmt->bindParam(':groupId',$groupId,PDO::PARAM_INT);
+            $stmt->bindParam(':planningId',$planningId,PDO::PARAM_INT);
+            $stmt->execute(); 
+            return $groupPlanning;
+    }
+    
+        catch (Exception $e){
+            exit('<b>Catched exception at line '. $e->getLine() .' :</b> '. $e->getMessage());
+        }
     }
 }
 
