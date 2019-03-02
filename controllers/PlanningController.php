d2a1227c549ddaca07c3bf5dd679090f00259e77
onzol <onzol@LAPTOP-GDLSB58Q>
2019-03-02 20:50:46 +0100

    Modification/correction fonction addEvent

--- controllers/PlanningController.php
+++ controllers/PlanningController.php
@@ -23,19 +23,17 @@ class PlanningController extends Controller{
     public function addEvent(){
         $planningManager=new PlanningManager();
             if(!empty($_POST['title']) AND !empty($_POST['start']) AND !empty($_POST['end']) ){
-                $newPlanning=new Planning(['title'=>$_POST['title'],'start'=>$_POST['start'],'end'=>$_POST['end']]);
+                $newPlanning=new Planning(['title'=>$_POST['title']." ".$_POST['teacher'],'start'=>$_POST['start'],'end'=>$_POST['end'],'userId'=>$_POST['userId']]);
                 var_dump($newPlanning);
                 $planningManager->createPlanning($newPlanning);
-                $planning=$planningManager->readPlanning();
-        $this->smarty->assign('planning', $planning);
-        $this->smarty->display('view/calendar.tpl');
-              
-
-                
-                
-    }else{
-            $this->smarty->display('view/planningForm.tpl');
-    }
+                $this->smarty->assign('newPlanning', $newPlanning);
+                $groupManager=new GroupManager();  
+                $groups=$groupManager->readAllGroups();
+                $this->smarty->assign('groups', $groups);
+                $this->smarty->display('view/groupPlanningForm.tpl');           
+            }else{
+                $this->smarty->display('view/planningForm.tpl');
+            }
     
     }
     
