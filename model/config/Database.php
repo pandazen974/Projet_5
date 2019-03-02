0107f799ad735cfa7ef95aab06a42fdff964bd51
onzol <onzol@LAPTOP-GDLSB58Q>
2019-03-02 20:27:44 +0100

    Ajout l.17 pour encodage UTF8

--- model/config/Database.php
+++ model/config/Database.php
@@ -14,6 +14,7 @@ class Database{
   
         try{
             $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION ));
+            $this->conn->exec("SET NAMES 'UTF8'");
         }catch(PDOException $exception){
             echo "Connection error: " . $exception->getMessage();
         }
