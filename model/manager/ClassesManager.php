53cdc9857643eca872c1fb22b5e1c97ab8cb400a
onzol <onzol@LAPTOP-GDLSB58Q>
2019-03-02 07:20:29 +0100

    Fonction readAllClasses l.32 permet la lecture des cours pour planning

--- model/manager/ClassesManager.php
+++ model/manager/ClassesManager.php
@@ -1,31 +1,66 @@
 <?php
 
-Class ClassesManager{
+Class ClassesManager extends Database{
     
-    private $conn;
     private $table_name = "classes";
+    protected $conn;
  
-    public function __construct($db){
-        $this->conn = $db;
+    public function __construct(){
+        $this->conn=parent::getConnection();
     }
     
-        public function createOneClass(Classes $classes){
-        $query = "INSERT INTO
+    public function createOneClass(Classes $classes){
+         try{
+            $query = "INSERT INTO
                     " . $this->table_name . "
                 SET
-                    className=:className, ";
+                    className=:className ";
  
-        $stmt = $this->conn->prepare($query);
-        $className= htmlspecialchars($classes->className());
-        $stmt->bindParam(':className',$className);
-        $stmt->execute();    
+            $stmt = $this->conn->prepare($query);
+            $className= htmlspecialchars($classes->className());
+            $stmt->bindParam(':className',$className,PDO::PARAM_STR);
+            $stmt->execute();    
+            $classes->setClassName($this->conn->lastInsertId(),PDO::PARAM_STR);
+            return $classes;
+         }
+        
+        catch (Exception $e){
+            exit('<b>Catched exception at line '. $e->getLine() .' :</b> '. $e->getMessage());
+        }
+    }
+    
+    public function readAllClasses(){
+        try{
+            $query = "SELECT *
+
+            FROM
+                " . $this->table_name . "
+            ORDER BY
+                id
+           ";
+
+            $stmt = $this->conn->prepare( $query );
+            $stmt->execute();
+
+            while($donnees=$stmt->fetch(\PDO::FETCH_ASSOC))
+                {
+                    $classes[]=new Classes($donnees);
+                }
+            return $classes;
+
+            }
+    
+        catch (Exception $e){
+            exit('<b>Catched exception at line '. $e->getLine() .' :</b> '. $e->getMessage());
+        }
+    
     }
     
     public function deleteOneClass(Classes $classes){
-    $query = "DELETE FROM classes WHERE id = :id";
-    $stmt = $this->conn->prepare($query);
-    $id=$classes->id();
-    $stmt->bindParam(':id', $id);
-    $stmt->execute();
+        $query = "DELETE FROM classes WHERE id = :id";
+        $stmt = $this->conn->prepare($query);
+        $id=$classes->id();
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
     }
 }
