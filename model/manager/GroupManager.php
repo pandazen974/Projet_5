942b090da9f9589777f4b01cc3a96347f1912464
onzol <onzol@LAPTOP-GDLSB58Q>
2019-03-02 20:43:46 +0100

    Ajout fonction readAllGroups pour récupérer tous les groupes

--- model/manager/GroupManager.php
+++ model/manager/GroupManager.php
@@ -1,12 +1,35 @@
 <?php
 
-Class GroupManager{
+Class GroupManager extends Database{
     
-    private $conn;
     private $table_name = "group";
+    protected $conn;
+    
  
-    public function __construct($db){
-        $this->conn = $db;
+    public function __construct(){
+        $this->conn=parent::getConnection();   
+    }
+    
+    public function readAllGroups(){
+        try{
+            $query = "SELECT * FROM `$this->table_name` ";
+
+            $stmt = $this->conn->prepare( $query );
+            $stmt->execute();
+
+            while($donnees=$stmt->fetch(\PDO::FETCH_ASSOC))
+                {
+                    $groups[]=new Group($donnees);
+                }
+
+            return $groups;
+
+            }
+    
+        catch (Exception $e){
+            exit('<b>Catched exception at line '. $e->getLine() .' :</b> '. $e->getMessage());
+        }
+    
     }
 }
 
