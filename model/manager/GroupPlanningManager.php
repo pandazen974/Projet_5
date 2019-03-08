1e9caa7f2c14b6f6b3feb26d8860491e0b28ed0f
onzol <onzol@LAPTOP-GDLSB58Q>
2019-03-08 09:27:46 +0100

    Ajout fonction updateGroupPlanning/deleteGroupPlanning

--- model/manager/GroupPlanningManager.php
+++ model/manager/GroupPlanningManager.php
@@ -30,5 +30,62 @@ Class GroupPlanningManager extends Database{
             exit('<b>Catched exception at line '. $e->getLine() .' :</b> '. $e->getMessage());
         }
     }
+    
+    public function updateGroupPlanning(GroupPlanning $groupPlanning){
+        try{
+            $query = "INSERT INTO
+                    " . $this->table_name . "
+                SET
+                    groupId=:groupId, planningId=:planningId";
+ 
+            $stmt = $this->conn->prepare($query);
+            $groupId= htmlspecialchars($groupPlanning->groupId());
+            $planningId= htmlspecialchars($groupPlanning->planningId());
+            $stmt->bindParam(':groupId',$groupId,PDO::PARAM_INT);
+            $stmt->bindParam(':planningId',$planningId,PDO::PARAM_INT);
+            $stmt->execute(); 
+            return $groupPlanning;
+    }
+    
+        catch (Exception $e){
+            exit('<b>Catched exception at line '. $e->getLine() .' :</b> '. $e->getMessage());
+        }
+        
+        
+    }
+    
+    public function readSelectedGroupPlanning($planningId){
+            $query = "SELECT *
+            FROM
+                " . $this->table_name . "
+            WHERE
+                planningId = :planningId
+            LIMIT
+                0,1";
+   
+            $stmt = $this->conn->prepare( $query );
+            $stmt->bindParam(':planningId', $planningId,PDO::PARAM_INT);
+            $stmt->execute();
+            $row = $stmt->fetch(\PDO::FETCH_ASSOC);
+            if($row===false){
+            $selectedGroupPlanning=null;
+            }else{
+            $selectedGroupPlanning=new GroupPlanning($row);
+            }
+            return $selectedGroupPlanning;
+            }
+    
+    public function deleteGroupPlanning(GroupPlanning $groupPlanning){
+        try{
+            $query = "DELETE FROM groupPlanning WHERE planningId = :planningId";
+            $stmt = $this->conn->prepare($query);
+            $planningId=$groupPlanning->planningId();
+            $stmt->bindParam(':planningId', $planningId,PDO::PARAM_INT);
+            $stmt->execute();
+        }
+        catch (Exception $e){
+            exit('<b>Catched exception at line '. $e->getLine() .' :</b> '. $e->getMessage());
+        }
+    }
 }
 
