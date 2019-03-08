16d9a5a6d1858364f6e13243314f22bcfe3c704a
onzol <onzol@LAPTOP-GDLSB58Q>
2019-03-08 09:43:56 +0100

    Ajout des propriétés pour style et rendre un cours cliquable

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,3 +1,5 @@
+/* global modal */
+
 var edt=
 {
     
@@ -21,23 +23,76 @@ var edt=
             start:'07:00:00',
             allDaySlot:false,
             minTime:"07:00",
-            maxTime:"18:00",
+            maxTime:"20:00",
             eventSources: [
             // your event source
             {
                 url: '/Projet_5/?action=feedEvents', // use the `url` property
-                color: 'yellow',    // an option!
+                color: 'green',    // an option!
                 textColor: 'black'  // an option!
             }
+            
+            
 
     // any other sources...
 
-  ]
+  ],
+  eventClick: function(calEvent) {
+
+    
+    
+    
+    $("#title").text(calEvent.title);
+    $("#modal").show();
+    document.getElementById('edit_link').href= 'http://localhost/Projet_5/index.php?action=openUpdatePlanningForm&id='+calEvent.id;
+    document.getElementById('delete_link').href= 'http://localhost/Projet_5/index.php?action=erasePlanning&id='+calEvent.id;
+  },
+  
+  eventMouseover: function(calEvent) {
+    $(this).css('border', 'solid 2px rgb(94, 255, 161)');  
+     $(this).css('cursor', 'pointer');  
+  },
+  
+  eventMouseout: function(calEvent) {
+    $(this).css('border', 'transparent');  
+    
+  },
+  
+  customButtons: {
+    addButton: {
+      text: '+Ajouter',
+      click: function() {
+        window.open('http://localhost/Projet_5/index.php?action=openPlanningForm');
+      }
+    }
+  },
+  header: {
+    left: 'prev,next today addButton',
+    center: 'title',
+    right: 'month,agendaWeek'
+  }
+  
+  
+  
+  
       // other view-specific options here
     
   });
- 
-  
+    window.onclick = function(event) {
+        var modal = document.getElementById('modal');
+  if (event.target === modal) {
+    $("#modal").hide();
+  }
+    };
+    
+    $( "#close" ).click(function() {
+    $("#modal").hide();
+    });
+    
+    
+    
+    
+    
   }
   
   
