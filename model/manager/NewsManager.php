65b0f59fe9021dc4b2b9b999fc5531a807b4b3e9
onzol <onzol@LAPTOP-GDLSB58Q>
2019-03-19 16:51:19 +0100

    Ajout fonctions createNews/readAllNews

--- model/manager/NewsManager.php
+++ model/manager/NewsManager.php
@@ -1,12 +1,68 @@
 <?php
 
-Class NewsManager{
+Class NewsManager extends Database{
     
-    private $conn;
     private $table_name = "news";
+    protected $conn;
  
-    public function __construct($db){
-        $this->conn = $db;
+    public function __construct(){
+        $this->conn=parent::getConnection();
+       
+    }
+    
+    public function createNews(News $news){
+       
+        try{
+            $query = "INSERT INTO
+                    " . $this->table_name . "
+                SET
+                 title=:title, content=:content, imageName=:imageName, imageDescription=:imageDescription, newsDate=NOW()";
+            $stmt = $this->conn->prepare($query);
+            $title= htmlspecialchars($news->title());
+            $content= htmlspecialchars($news->content());
+            $imageName= htmlspecialchars($news->imageName());
+            $imageDescription= htmlspecialchars($news->imageDescription());
+            $stmt->bindParam(':title',$title,PDO::PARAM_STR);
+            $stmt->bindParam(':content',$content,PDO::PARAM_STR);
+            $stmt->bindParam(':imageName',$imageName,PDO::PARAM_STR);
+            $stmt->bindParam(':imageDescription',$imageDescription,PDO::PARAM_STR);
+           $stmt->execute();
+            var_dump($news);
+            return $news;
+        }
+            
+        catch (Exception $e){
+            exit('<b>Catched exception at line '. $e->getLine() .' :</b> '. $e->getMessage());
+        }
+        
+        }
+    
+    
+        public function readAllNews(){
+        try{
+            $query = "SELECT *,DATE_FORMAT(newsDate, '%d/%m/%Y') as newsDate
+
+            FROM
+                " . $this->table_name . "
+            ORDER BY
+                id
+           ";
+
+            $stmt = $this->conn->prepare( $query );
+            $stmt->execute();
+
+            while($donnees=$stmt->fetch(\PDO::FETCH_ASSOC))
+                {
+                    $news[]=new News($donnees);
+                }
+                var_dump($news);
+            return $news;
+
+            }
+    
+        catch (Exception $e){
+            exit('<b>Catched exception at line '. $e->getLine() .' :</b> '. $e->getMessage());
+        }
+    
     }
 }
-
