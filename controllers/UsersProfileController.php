63f24e66f513a830bb1206c344c1057bd607f744
onzol <onzol@LAPTOP-GDLSB58Q>
2019-04-11 10:50:06 +0200

    adaptation du code, choix plus large des profils

--- controllers/UsersProfileController.php
+++ controllers/UsersProfileController.php
@@ -11,17 +11,17 @@ public function registerUsersProfile(){
     $usersProfileManager=new UsersProfileManager();
     
                 $usersProfile=new UsersProfile(['userId'=>$_GET['id'],'profileId'=>$_POST['profile']]);
-                if(($_POST['profile']==="1") OR ($_POST['profile']==="2")){
+                if($_POST['profile']==="Etudiant") {
+                    $newUserProfile=$usersProfileManager->setUsersProfile($usersProfile);
+                    $this->smarty->assign('newUserProfile', $newUserProfile);
+                    var_dump($newUserProfile);
+                    $this->smarty->display('view/groupForm.tpl');
+                }else{
                     $userManager=new UserManager();
                     $newUserProfile=$usersProfileManager->setUsersProfile($usersProfile);
                     $users=$userManager->readAllUsers();
                     $this->smarty->assign('users', $users);
                     $this->smarty->display('view/usersList.tpl'); 
-                }else{
-                    $newUserProfile=$usersProfileManager->setUsersProfile($usersProfile);
-                    $this->smarty->assign('newUserProfile', $newUserProfile);
-                    var_dump($newUserProfile);
-                    $this->smarty->display('view/groupForm.tpl');
                 }
     }
     
