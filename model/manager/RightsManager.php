e22348fdb4dc93dd1838ce687407c2df8a11c8df
onzol <onzol@LAPTOP-GDLSB58Q>
2019-04-11 11:02:12 +0200

    fonction readAllRights ok

--- model/manager/RightsManager.php
+++ model/manager/RightsManager.php
@@ -1,12 +1,39 @@
 <?php
 
-Class RightsManager{
+Class RightsManager extends Database{
     
-    private $conn;
     private $table_name = "rights";
+    protected $conn;
  
-    public function __construct($db){
-        $this->conn = $db;
+    public function __construct(){
+        $this->conn=parent::getConnection();
     }
+    
+    public function readAllRights(){
+        try{
+            $query = "SELECT *
+
+            FROM
+                " . $this->table_name . "
+            ORDER BY
+                id
+           ";
+
+            $stmt = $this->conn->prepare( $query );
+            $stmt->execute();
+
+            while($donnees=$stmt->fetch(\PDO::FETCH_ASSOC))
+                {
+                    $rights[]=new Rights($donnees);
+                }
+
+            return $rights;
+
+            }
+    
+        catch (Exception $e){
+            exit('<b>Catched exception at line '. $e->getLine() .' :</b> '. $e->getMessage());
+        }
 }
 
+}
