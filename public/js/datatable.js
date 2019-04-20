a30f382b7b9d5cd56ce66020a18c85518d8265be
onzol <onzol@LAPTOP-GDLSB58Q>
2019-04-20 21:23:44 +0200

    Modifie pour le responsive

--- public/js/datatable.js
+++ public/js/datatable.js
@@ -8,13 +8,32 @@ $(document).ready(function () {
 
     $('#table_id').DataTable({
         
+        responsive:true,
+        columns: [
+        { responsivePriority: 1 },
+        { responsivePriority: 11 },
+        { responsivePriority: 12 },
+        { responsivePriority: 3 },
+        { responsivePriority: 4 },
+        { responsivePriority: 5 },
+        { responsivePriority: 6 },
+        { responsivePriority: 7 },
+        { responsivePriority: 8 },
+        { responsivePriority: 9 },
+        { responsivePriority: 10 },
         
+        
+        
+        { responsivePriority: 2 }
+        
+    ],
+        scrollX: false,
         language: {
 
             url: "/Projet_5/public/js/french.json"
 
         }
-        
+         
 
     });
 
