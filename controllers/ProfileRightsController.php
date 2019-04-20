760a37e177b06ddcf32dfff3b2ad461af3c92c3f
onzol <onzol@LAPTOP-GDLSB58Q>
2019-04-20 23:12:29 +0200

    Indentation code à revoir redirection page accueil

--- controllers/ProfileRightsController.php
+++ controllers/ProfileRightsController.php
@@ -15,16 +15,59 @@ class ProfileRightsController extends Controller{
         foreach($_POST['cases'] as $value)
             
     {
-            var_dump($value);
-        
+            
         $profileRight= new ProfileRights(['profileId'=>$_GET['id'], 'rightsId'=>$value]);
         $profileRights= $profileRightsManager->createProfileRights($profileRight);
     }
       
-         
-        $this->smarty->assign('profileRights',$profileRights);
-        var_dump($profileRights);
-        $this->smarty->display('view/home.tpl');
+    $this->smarty->assign('profileRights',$profileRights);
+    $this->smarty->display('view/home.tpl');
+    }
+    
+    public function showProfileRights(){
+        $profileRightsManager=new ProfileRightsManager();
+        $cases=$profileRightsManager->selectProfileRights($_POST['profile']);
+        $rightsManager=new RightsManager();
+        $allRights=$rightsManager->readAllRights();  
+        $this->smarty->assign('allRights',$allRights);
+        $this->smarty->assign('cases',$cases);
+        $this->smarty->display('view/updateRightsForm.tpl');
+        
+    }
+    
+    public function modifyProfileRights(){
+        
+     $profileRightsManager=new ProfileRightsManager();
+     $profileRights=$profileRightsManager->getSelectedProfileRights($_GET['id']);
+     $profileRightsManager->deleteProfileRights($profileRights);
+     foreach($_POST['newRights'] as $value)      
+    {
+        $profileRight= new ProfileRights(['profileId'=>$_GET['id'], 'rightsId'=>$value]);
+        $newProfileRights= $profileRightsManager->createProfileRights($profileRight);
+    }
+       
+    $this->smarty->assign('newProfileRights',$newProfileRights);
+    $this->smarty->display('view/home.tpl');
+    }
+    
+    public function openEraseProfileForm(){
+        $profileManager=new ProfileManager;
+        $profiles=$profileManager->readAllProfile();
+        $this->smarty->assign('profiles',$profiles);
+        $this->smarty->display('view/deleteProfileForm.tpl');
+    }
+    
+    public function eraseProfileRights(){
+     $profileRightsManager=new ProfileRightsManager();
+     $profileRights=$profileRightsManager->getSelectedProfileRights($_POST['profile']);
+     $profileRightsManager->deleteProfileRights($profileRights);
+     $profileManager=new ProfileManager();
+     $profile=$profileManager->getSelectedProfile($_POST['profile']);
+     $profileManager->deleteProfile($profile); 
+     $profiles=$profileManager->readAllProfile();
+     $this->smarty->assign('profiles',$profiles);
+     $this->smarty->display('view/deleteProfileForm.tpl');
+   
     }
 }
 
