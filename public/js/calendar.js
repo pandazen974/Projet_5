a86f64ad687c22f44accdf72d293602309896680
onzol <onzol@LAPTOP-GDLSB58Q>
2019-04-20 21:24:04 +0200

    Modifie pour le responsive

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -20,19 +20,26 @@ var edt=
                 },
             defaultView: 'agendaWeek',
             titleFormat:'D MMMM YYYY',
-            start:'07:00:00',
             allDaySlot:false,
-            minTime:"07:00",
-            maxTime:"20:00",
+            minTime:'07:00:00',
+            maxTime:'20:00:00',
             eventSources: [
             // your event source
             {
-                url: '/Projet_5/?action=feedEvents', // use the `url` property
-                color: 'rgba(1, 50, 67, 1)',    // an option!
+                url: '/Projet_5/?action=feedGroupOneEvents', // use the `url` property
+                color: 'rgba(0, 230, 64, 1)',    // an option!
+                textColor: 'black'  // an option!
+            },
+            {
+                url: '/Projet_5/?action=feedGroupTwoEvents', // use the `url` property
+                color: 'rgba(77, 19, 209, 1)',    // an option!
+                textColor: 'black'  // an option!
+            },
+            {
+                url: '/Projet_5/?action=feedGroupThreeEvents', // use the `url` property
+                color: 'rgba(240, 255, 0, 1)',    // an option!
                 textColor: 'black'  // an option!
             }
-            
-            
 
     // any other sources...
 
@@ -48,17 +55,6 @@ var edt=
     document.getElementById('delete_link').href= 'http://localhost/Projet_5/index.php?action=erasePlanning&id='+calEvent.id;
   },
   
-  eventMouseover: function() {
-    $(this).css('border', 'solid 2px rgba(107, 185, 240, 1)'); 
-    $(this).css('background-color', 'rgba(37, 116, 169, 1)');
-    $(this).css('cursor', 'pointer');  
-  },
-  
-  eventMouseout: function() {
-    $(this).css('border', 'transparent');  
-    $(this).css('background-color', 'rgba(1, 50, 67, 1)');
-    
-  },
   
   customButtons: {
     addButton: {
@@ -91,6 +87,10 @@ var edt=
     $("#modal").hide();
     });
     
+    if($('#status').text()!=="Etudiant"){
+        $('.fc-addButton-button').css('visibility','visible');
+    }
+
     
     
     
