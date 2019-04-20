5f163c6488d14c96672a56b6f486f365bd181416
onzol <onzol@LAPTOP-GDLSB58Q>
2019-04-20 23:15:55 +0200

    Ajout des fonctions pour alimenter jsons differents  groupes ok

--- controllers/PlanningController.php
+++ controllers/PlanningController.php
@@ -13,17 +13,33 @@ class PlanningController extends Controller{
                 
     }
     
-    public function feedEvents(){
-        $planningManager=new PlanningManager();
-        $planning=$planningManager->readPlanning();
-        $this->smarty->assign('planning', $planning); 
-        $this->smarty->display('view/feed.tpl');
+    public function feedGroupOneEvents(){
+        $groupPlanningManager=new GroupPlanningManager();
+        $planningOne=$groupPlanningManager->readPlanningOne();
+        $this->smarty->assign('planningOne', $planningOne); 
+        $this->smarty->display('view/groupOne.tpl');
+    }
+    
+     public function feedGroupTwoEvents(){
+        $groupPlanningManager=new GroupPlanningManager();
+        $planningTwo=$groupPlanningManager->readPlanningTwo();
+        $this->smarty->assign('planningTwo', $planningTwo); 
+        $this->smarty->display('view/groupTwo.tpl');
+    }
+    
+     public function feedGroupthreeEvents(){
+        $groupPlanningManager=new GroupPlanningManager();
+        $planningThree=$groupPlanningManager->readPlanningThree();
+        $this->smarty->assign('planningThree', $planningThree); 
+        $this->smarty->display('view/groupThree.tpl');
     }
     
     public function addEvent(){
         $planningManager=new PlanningManager();
             if(!empty($_POST['title']) AND !empty($_POST['start']) AND !empty($_POST['end']) ){
-                $newPlanning=new Planning(['title'=>$_POST['title']."\n". $_POST['teacher'],'start'=>$_POST['start'],'end'=>$_POST['end']]);
+                $start=date('Y-m-d H:i:s', strtotime($_POST['start']));
+                $end=date('Y-m-d H:i:s', strtotime($_POST['end']));
+                $newPlanning=new Planning(['title'=>$_POST['title']."\n". $_POST['teacher'],'start'=>$start,'end'=>$end]);
                 var_dump($newPlanning);
                 $planningManager->createPlanning($newPlanning);
                 $this->smarty->assign('newPlanning', $newPlanning);
@@ -40,6 +56,10 @@ class PlanningController extends Controller{
     public function openUpdatePlanningForm(){
         $planningManager=new PlanningManager();
         $planning=$planningManager->readSelectedPlanning($_GET['id']);
+        var_dump($planning);
+        $planning->setStart(date('d-m-Y H:i:s', strtotime($planning->start())));
+        $planning->setEnd(date('d-m-Y H:i:s', strtotime($planning->end())));
+        date('Y-m-d H:i:s', strtotime($planning->start()));
         $this->smarty->assign('planning', $planning);
         $classesManager=new ClassesManager();
         $classes=$classesManager->readAllClasses();
@@ -55,19 +75,25 @@ class PlanningController extends Controller{
     }
     
     public function modifyPlanning(){
-           $groupManager=new GroupManager();
+        $groupManager=new GroupManager();
         $groups=$groupManager->readAllGroups();
         $this->smarty->assign('groups', $groups);
+        //suppression de la relation groupe/Planning
         $groupPlanningManager=new GroupPlanningManager();
-        $groupPlanning=new GroupPlanning(['groupId'=>$_POST['groupName'],'planningId'=>$_GET['id']]);
-        var_dump($groupPlanning);
-        $groupPlanningManager->updateGroupPlanning($groupPlanning);
+        $groupPlanning=$groupPlanningManager->readSelectedGroupPlanning($_GET['id']);
+        $groupPlanningManager->deleteGroupPlanning($groupPlanning);
+        //suppression du Planning concerné
         $planningManager=new PlanningManager();
-        $newPlanning=new Planning(['id'=>$_GET['id'],'title'=>$_POST['title']."\n". $_POST['teacher'],'start'=>$_POST['start'],'end'=>$_POST['end']]);
-        $planningManager->updatePlanning($newPlanning);
+        $planning=$planningManager->readSelectedPlanning($_GET['id']);
+        $planningManager->deletePlanning($planning);
+        //création d'un nouveau planning
+        $start=date('Y-m-d H:i:s', strtotime($_POST['start']));
+        $end=date('Y-m-d H:i:s', strtotime($_POST['end']));
+        $newPlanning=new Planning(['title'=>$_POST['title']."\n". $_POST['teacher'],'start'=>$start,'end'=>$end]);
+        $planningManager->createPlanning($newPlanning);
         var_dump($newPlanning);
-        $this->smarty->assign('$newPlanning', $newPlanning);
-        $this->smarty->display('view/calendar.tpl');
+        $this->smarty->assign('newPlanning', $newPlanning);
+        $this->smarty->display('view/newGroupPlanningForm.tpl');
     }
     
     public function erasePlanning(){
