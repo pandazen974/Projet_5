9e2adeba5a5dd02cdd647f6ea71646c825d1cb6d
onzol <onzol@LAPTOP-GDLSB58Q>
2019-04-20 23:14:03 +0200

    Ajout fonction listAllProfile

--- controllers/ProfileController.php
+++ controllers/ProfileController.php
@@ -12,6 +12,13 @@ class ProfileController extends Controller{
         $this->smarty->display('view/newProfile.tpl');
     }
     
+    public function listAllProfile(){
+        $profileManager=new ProfileManager;
+        $profiles=$profileManager->readAllProfile();
+        $this->smarty->assign('profiles',$profiles);
+        $this->smarty->display('view/profileListForm.tpl');
+    }
+    
     public function addProfile (){
         $profileManager=new ProfileManager;
         $profile=new Profile(['profileName'=>$_POST['profile']]);
@@ -22,5 +29,7 @@ class ProfileController extends Controller{
         $this->smarty->assign('allRights',$allRights);
         $this->smarty->display('view/rights.tpl');
     }
+    
+ 
        
 }
