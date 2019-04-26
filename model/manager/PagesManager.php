b5c97706d9e220301e64ff14740016fff2eea090
onzol <onzol@LAPTOP-GDLSB58Q>
2019-04-26 20:06:55 +0200

    Ajout PagesManager pour gestion des pages fction read/update ok

---
+++ model/manager/PagesManager.php
@@ -0,0 +1,76 @@
+<?php
+
+class PagesManager extends Database{
+ 
+    private $table_name = "pages";
+    protected $conn;
+    
+ 
+   public function __construct(){
+        $this->conn=parent::getConnection();
+       
+    }
+    
+public function readAllPages(){
+    $query = "SELECT *
+
+            FROM
+                " . $this->table_name . "
+            ORDER BY
+                id
+           ";
+
+    $stmt = $this->conn->prepare( $query );
+    $stmt->execute();
+
+    while($donnees=$stmt->fetch(\PDO::FETCH_ASSOC))
+    {
+        $pages[]=new Pages($donnees);
+    }
+
+    return $pages;
+
+    
+}
+
+public function readPageWithId($id){
+   $query = "SELECT *
+            FROM
+                " . $this->table_name . "
+            WHERE
+                id = :id
+            LIMIT
+                0,1";
+   
+            $stmt = $this->conn->prepare( $query );
+            $stmt->bindParam(':id', $id,PDO::PARAM_INT);
+            $stmt->execute();
+            $row = $stmt->fetch(\PDO::FETCH_ASSOC);
+            if($row===false){
+            $selectedPage=null;
+            }else{
+            $selectedPage=new Pages($row);
+            }
+            return $selectedPage;
+            }
+
+public function updatePage(Pages $pages){
+    try{
+            $query = "UPDATE pages
+                    SET
+                    content=:content
+                    WHERE id=:id";
+            $stmt = $this->conn->prepare($query);
+            $content= $pages->content();
+            $id= $pages->id();
+            $stmt->bindParam(':content',$content,PDO::PARAM_STR);
+            $stmt->bindParam(':id',$id,PDO::PARAM_INT);
+            $stmt->execute();
+            return $pages;
+            }
+            
+        catch (Exception $e){
+            exit('<b>Catched exception at line '. $e->getLine() .' :</b> '. $e->getMessage());
+        }
+}
+}
